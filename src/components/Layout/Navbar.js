d3606c76dc877452c0efe1154eced788f20abe31
Nafi R <saejim91@gmail.com>
2022-11-17 20:28:11 +0600

    navbar styling done

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,39 +1,71 @@
 import React, { Component } from 'react';
 import logo from '../assets/a-logo.png';
+import classes from './Navbar.module.css';
 
 class Navbar extends Component {
 	render() {
 		return (
-			<header className="header">
-				<nav className="navigation">
-					<ul className="navigation header-navigation">
-						<li className="navigation header-navigation navigation-item">
+			<header className={classes.header}>
+				<nav className={classes.navigation}>
+					<ul className={`navigation ${classes['header-navigation']}`}>
+						<li
+							className={`navigation header-navigation ${classes['navigation-item']}`}
+						>
 							Women
 						</li>
-						<li className="navigation header-navigation navigation-item">
+						<li
+							className={`navigation header-navigation ${classes['navigation-item']}`}
+						>
 							Men
 						</li>
-						<li className="navigation header-navigation navigation-item">
+						<li
+							className={`navigation header-navigation ${classes['navigation-item']}`}
+						>
 							Kids
 						</li>
 					</ul>
 				</nav>
-				<img src={logo} alt="Brand logo" className="header-logo" />
 
-				<div className="header actions">
-					<div className="header action-dropdown container">
-						<span className="header action-dropdown currency-symbol">$</span>
-						<ul className="header action-dropdown ">
-							<li className="header action-dropdwon item">$ USD</li>
-							<li className="header action-dropdwon item">&euro;EURO</li>
-							<li className="header action-dropdwon item">&#165;JPY</li>
+				<div className={`header-logo ${classes['logo-container']}`}>
+					<img src={logo} alt="Brand logo" className={classes['header-logo']} />
+				</div>
+
+				<div className={classes['header-actions']}>
+					<div className={`header action-dropdown ${classes.container} `}>
+						<div
+							className={`header action-dropdown ${classes['currency-symobol-container']} `}
+						>
+							<span className="header action-dropdown currency-symbol">$</span>
+
+							<span
+								className={`header action-dropdown ${classes['dropdown-symbol']}`}
+							>
+								&#8964;
+							</span>
+						</div>
+						<ul className={`header ${classes['action-dropdown-container']} `}>
+							<li
+								className={`header action-dropdwon ${classes['dropdown-item']}`}
+							>
+								$ USD
+							</li>
+							<li
+								className={`header action-dropdwon ${classes['dropdown-item']}`}
+							>
+								&euro;EURO
+							</li>
+							<li
+								className={`header action-dropdwon ${classes['dropdown-item']}`}
+							>
+								&#165;JPY
+							</li>
 						</ul>
-						<span className="header action-dropdown dropdown-symbol">
-							&#8964;
-						</span>
 					</div>
 
-					<span className="header actions-cartbtn" role="button">
+					<span
+						className={`header ${classes['actions-cartbtn']}`}
+						role="button"
+					>
 						&#x1F6D2;
 					</span>
 				</div>
