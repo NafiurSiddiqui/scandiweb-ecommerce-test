f5b4f616bbabca62b6bfcc5339d3b65a07385579
Nafi R <saejim91@gmail.com>
2022-12-15 04:52:37 +0600

    PDP:btnItem logic 1/2 completed

--- src/components/UI/DescriptionCard.jsx
+++ src/components/UI/DescriptionCard.jsx
@@ -8,25 +8,28 @@ export default class DescriptionCard extends Component {
 	constructor() {
 		super();
 		this.state = {
-			itemIsClicked: false,
+			activeItem: '',
 		};
 		this.itemClickHandler = this.itemClickHandler.bind(this);
 	}
 
-	itemClickHandler(e) {
+	itemClickHandler(e, item) {
 		if (e.target.dataset.clicked) {
 			e.target.dataset.clicked = true;
-			e.target.style.backgroundColor = 'red';
+			// e.target.style.backgroundColor = 'red';
 		}
-		console.log(e.target.dataset.clicked);
-		return;
+		// console.log(e.target.dataset.clicked);
+		// console.log(e);
+
+		this.setState({
+			activeItem: item,
+		});
 	}
 
 	render() {
 		const { brand, name, attributesID, attributesItem, prices } =
 			this.props.products[0];
-		// console.log(this.state.itemIsClicked);
-
+		// console.log(this.state.activeItem);
 		return (
 			<article className="pd">
 				<div className="pd__headers">
@@ -39,17 +42,23 @@ export default class DescriptionCard extends Component {
 							<li key={element} className="pd__attribution">
 								<span className="pd__attribution__ID">{element}</span>
 								<ul className="pd__attribution__items">
-									{attributesItem[i].map((item) => (
-										<li
-											className="pd__attribution__item"
-											role={'button'}
-											key={item}
-											data-clicked={false}
-											onClick={this.itemClickHandler}
-										>
-											{item}
-										</li>
-									))}
+									{attributesItem[i].map((item) => {
+										let itemMatched = item === this.state.activeItem;
+
+										return (
+											<li
+												className={`pd__attribution__item ${
+													itemMatched ? 'itemActive' : ''
+												}`}
+												role={'button'}
+												key={item}
+												data-clicked={false}
+												onClick={(e) => this.itemClickHandler(e, item)}
+											>
+												{item}
+											</li>
+										);
+									})}
 								</ul>
 							</li>
 						</ul>
