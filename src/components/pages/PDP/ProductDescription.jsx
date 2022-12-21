1a0e60eecd9ad0f5d12265c3b1b19f7209809c0d
Nafi R <saejim91@gmail.com>
2022-12-22 01:02:16 +0600

    Reusable modal component initiated

--- src/components/pages/PDP/ProductDescription.jsx
+++ src/components/pages/PDP/ProductDescription.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import DescriptionCard from '../../UI/DescriptionCard';
+import DescriptionCard from './DescriptionCard';
 import Button from '../../UI/Button';
 import { connect } from 'react-redux';
 import { GET_ALL_CATEGORIES } from '../Category/CategoryList';
