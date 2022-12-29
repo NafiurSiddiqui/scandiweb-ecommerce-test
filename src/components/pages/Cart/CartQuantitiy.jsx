a7a3dd4a9158571078cb4781db3f780c574de397
Nafi R <saejim91@gmail.com>
2022-12-29 20:37:53 +0600

    quantity handler implemented

--- src/components/pages/Cart/CartQuantitiy.jsx
+++ src/components/pages/Cart/CartQuantitiy.jsx
@@ -3,20 +3,56 @@ import React, { Component } from 'react';
 import ProgressiveImage from '../../Utilities/ProgressiveImage';
 
 export default class CartQuantitiy extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			itemCount: 0,
+		};
+		this.increaseItemCount = this.increaseItemCount.bind(this);
+		this.decreaseItemCount = this.decreaseItemCount.bind(this);
+	}
+
+	increaseItemCount() {
+		this.setState({
+			itemCount: this.state.itemCount + 1,
+		});
+	}
+
+	decreaseItemCount() {
+		this.setState({
+			itemCount: this.state.itemCount - 1,
+		});
+	}
+
 	render() {
 		const { images, imageCount } = this.props;
+		const { itemCount } = this.state;
 
 		return (
 			<section className="cart-quantity-container">
-				<div className="cart-quantity-container__actions">
-					<div className="cart-quantity-container__actions-btn" role={'button'}>
-						<span className="cart-quantity-container__actions-btn-symbol">
+				<div
+					className="cart-quantity-container__actions"
+					aria-roledescription="Quantity button container"
+				>
+					<div className="cart-quantity-container__actions-btn">
+						<span
+							className="cart-quantity-container__actions-btn-symbol"
+							onClick={this.increaseItemCount}
+							role={'button'}
+						>
 							+
 						</span>
 					</div>
-					<span className="cart-quantity-container__actions-amount">1</span>
+					<span className="cart-quantity-container__actions-amount">
+						{itemCount}
+					</span>
 					<div className="cart-quantity-container__actions-btn">
-						<span className="cart-quantity-container__actions-btn-symbol">
+						<span
+							className="cart-quantity-container__actions-btn-symbol"
+							onClick={this.decreaseItemCount}
+							role={'button'}
+						>
 							-
 						</span>
 					</div>
