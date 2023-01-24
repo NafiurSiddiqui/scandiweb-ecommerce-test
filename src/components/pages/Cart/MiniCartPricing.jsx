57cc251f155301c9be82c00d8f596165cdd9f5f4
Nafi R <saejim91@gmail.com>
2023-01-24 21:49:20 +0600

    sepearate cartPricing component for cartItems

---
+++ src/components/pages/Cart/MiniCartPricing.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setMiniCartIsOpen } from '../../store/cartSlice';
+import Button from '../../UI/Button';
+
+class CartPricing extends Component {
+	render() {
+		const { className, cartTotal, currencySymbol, setMiniCartIsOpen } =
+			this.props;
+
+		return (
+			<section className={className}>
+				<div className={`${className} price-info`} role="contentinfo">
+					<span className={`${className} price-info__total`}>Total</span>
+					<span className={`${className} price-info__amount`}>
+						{currencySymbol ? currencySymbol : '$'}
+						{cartTotal}
+					</span>
+				</div>
+
+				<div className="cart-btns">
+					<Link
+						to="Cart"
+						className="cart-btns__btn view-bag"
+						onClick={() => setMiniCartIsOpen(false)}
+					>
+						VIEW BAG
+					</Link>
+
+					<Button className={`cart-btns__btn ${className} `}>CHECKOUT</Button>
+				</div>
+			</section>
+		);
+	}
+}
+
+const mapStateToProps = (state) => ({
+	cartTotal: state.cart.cartTotal,
+	currencySymbol: state.currency.selectedCurrency?.symbol,
+	miniCartIsOpen: state.cart.miniCartIsOpen,
+});
+
+export default connect(mapStateToProps, { setMiniCartIsOpen })(CartPricing);
