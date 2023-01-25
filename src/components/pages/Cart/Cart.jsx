fa385871c4e946523198f2d0495e30d0e2eeebc6
Nafi R <saejim91@gmail.com>
2023-01-26 00:57:23 +0600

    reusable content-wrapper created

--- src/components/pages/Cart/Cart.jsx
+++ src/components/pages/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ContentWrapper from '../../Layout/ContentWrapper';
 import DisplayHeader from '../../Layout/DisplayHeader';
 
 import CartContainer from './CartContainer';
@@ -6,14 +7,13 @@ import CartContainer from './CartContainer';
 /**
  * @Task -1. Cart item total quantity badge on the cart icon should display the total cart item quantity not the cart item count.
  */
-
 class Cart extends Component {
 	render() {
 		return (
-			<section>
+			<ContentWrapper>
 				<DisplayHeader>Cart</DisplayHeader>
 				<CartContainer className="cart-page" cartPage={true} />
-			</section>
+			</ContentWrapper>
 		);
 	}
 }
