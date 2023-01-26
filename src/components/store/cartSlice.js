a57436a069a73487c0248f18ddce714f21325b65
Nafi R <saejim91@gmail.com>
2023-01-27 01:28:32 +0600

    need to rethink and restructure the attribute item selection

--- src/components/store/cartSlice.js
+++ src/components/store/cartSlice.js
@@ -16,34 +16,30 @@ export const cartSlice = createSlice({
 		addItemToCart: (state, action) => {
 			const id = action.payload[0];
 			const items = action.payload[1];
-
-			//Recursive model
-			function recursiveCheck(items, newItemValues) {
-				items.forEach((item) => {
-					if (Array.isArray(item[1])) {
-						let subItemValues = [];
-						item[1].forEach((subitem) => {
-							if (subitem.hasOwnProperty('value')) {
-								subItemValues.push({
-									value: subitem.value,
-									isChecked: subitem.isChecked,
-								});
-							}
-						});
-						newItemValues.push({ name: item[0], values: subItemValues });
-					}
-				});
-			}
-
 			let newItemValues = [];
-			recursiveCheck(items, newItemValues);
+
+			console.log(items);
+
+			items.forEach((item) => {
+				if (Array.isArray(item[1])) {
+					let subItemValues = [];
+					item[1].forEach((subitem) => {
+						if (subitem.hasOwnProperty('value')) {
+							subItemValues.push({
+								value: subitem.value,
+								isChecked: subitem.isChecked,
+							});
+						}
+					});
+					newItemValues.push({ name: item[0], values: subItemValues });
+				}
+			});
 
 			let existingItem = state.cartItems.find(
 				(cartItem) =>
 					cartItem[0] === id &&
 					JSON.stringify(cartItem[1]) === JSON.stringify(newItemValues)
 			);
-
 			if (existingItem) {
 				existingItem[2].quantity++;
 			} else {
@@ -55,6 +51,7 @@ export const cartSlice = createSlice({
 				state.cartItems.push(newCartItem);
 			}
 		},
+
 		setMiniCartIsOpen: (state) => {
 			state.miniCartIsOpen = !state.miniCartIsOpen;
 		},
@@ -73,6 +70,30 @@ export const cartSlice = createSlice({
 				existingItem[2].quantity++;
 			}
 		},
+		// decrementItem: (state, action) => {
+		// 	const { cartItems } = state;
+		// 	const id = action.payload[0];
+		// 	const items = action.payload[1];
+
+		// 	let existingItem = cartItems.find(
+		// 		(cartItem) =>
+		// 			cartItem[0] === id &&
+		// 			JSON.stringify(cartItem[1]) === JSON.stringify(items)
+		// 	);
+
+		// 	if (existingItem) {
+		// 		//REMOVE IF 0
+		// 		if (existingItem[2].quantity === 1) {
+		// 			let removedCartItem = cartItems.filter((item) => item[0] !== id);
+
+		// 			return {
+		// 				...state,
+		// 				cartItems: removedCartItem,
+		// 			};
+		// 		}
+		// 		existingItem[2].quantity--;
+		// 	}
+		// },
 		decrementItem: (state, action) => {
 			const { cartItems } = state;
 			const id = action.payload[0];
@@ -87,7 +108,11 @@ export const cartSlice = createSlice({
 			if (existingItem) {
 				//REMOVE IF 0
 				if (existingItem[2].quantity === 1) {
-					let removedCartItem = cartItems.filter((item) => item[0] !== id);
+					let removedCartItem = cartItems.filter(
+						(item) =>
+							item[0] !== id ||
+							JSON.stringify(item[1]) !== JSON.stringify(items)
+					);
 
 					return {
 						...state,
@@ -97,6 +122,7 @@ export const cartSlice = createSlice({
 				existingItem[2].quantity--;
 			}
 		},
+
 		cartTotalHandler: (state, action) => {
 			let prices = action.payload;
 
