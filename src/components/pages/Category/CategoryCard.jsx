9c466ab242cb6325ecfb99f7e2f43c668922f465
Nafi R <saejim91@gmail.com>
2023-02-20 18:22:02 +0600

    content wrapper and styling fixed

--- src/components/pages/Category/CategoryCard.jsx
+++ src/components/pages/Category/CategoryCard.jsx
@@ -37,39 +37,41 @@ class CategoryCard extends Component {
 
 		return (
 			<li className={'category-item'} key={index}>
-				<div className={'category-item__image-wrapper'}>
-					<Link to={'/ProductDescription'}>
-						<div
-							className={'category-item__image-wrapper__image'}
-							style={{
-								backgroundImage: `url(${image})`,
-								...this.style,
-							}}
-							onClick={() => getProductID(productID)}
-						></div>
-						{!inStock ? (
-							<span
-								className={'category-item__image-wrapper-outOfStock'}
+				<div className="category-item-wrapper">
+					<div className={'category-item__image-wrapper'}>
+						<Link to={'/ProductDescription'}>
+							<div
+								className={'category-item__image-wrapper__image'}
+								style={{
+									backgroundImage: `url(${image})`,
+									...this.style,
+								}}
 								onClick={() => getProductID(productID)}
-							>
-								OUT OF STOCK
-							</span>
-						) : null}
-					</Link>
-					<MiniCartIcon
-						color={'#ffffff'}
-						className={`category-item__image-wrapper__cart`}
-						itemID={productID}
-						inStock={inStock}
-					/>
-				</div>
+							></div>
+							{!inStock ? (
+								<span
+									className={'category-item__image-wrapper-outOfStock'}
+									onClick={() => getProductID(productID)}
+								>
+									OUT OF STOCK
+								</span>
+							) : null}
+						</Link>
+						<MiniCartIcon
+							color={'#ffffff'}
+							className={`category-item__image-wrapper__cart`}
+							itemID={productID}
+							inStock={inStock}
+						/>
+					</div>
 
-				<div className="category-item__meta-container">
-					<h2>{heading}</h2>
-					<p>
-						<span>{currencySymbol ? currencySymbol : '$'}</span>
-						{price}
-					</p>
+					<div className="category-item__meta-container">
+						<h2>{heading}</h2>
+						<p>
+							<span>{currencySymbol ? currencySymbol : '$'}</span>
+							{price}
+						</p>
+					</div>
 				</div>
 			</li>
 		);
