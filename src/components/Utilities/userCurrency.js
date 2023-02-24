3dcadd6b1399a2d683a16d89d4bdc2ddbbb5d254
Nafi R <saejim91@gmail.com>
2023-02-24 23:08:36 +0600

    file renamed

---
+++ src/components/Utilities/userCurrency.js
@@ -0,0 +1,42 @@
+export function userCurrency(products, selectedCurrency, singleItem = false) {
+	let currencies;
+	let userMatchedCurrency;
+	//extracting only price items
+	if (!singleItem) {
+		currencies = products?.map((item) =>
+			item.prices.map((item) => {
+				return {
+					currency: item.currency.label,
+					symbol: item.currency.symbol,
+					amount: item.amount,
+				};
+			})
+		);
+
+		userMatchedCurrency = currencies?.map((item) =>
+			item.find((el) =>
+				selectedCurrency !== null
+					? el.currency === selectedCurrency.currency
+					: el.currency === 'USD'
+			)
+		);
+
+		return userMatchedCurrency;
+	} else {
+		currencies = products.prices.map((item) => {
+			return {
+				currency: item.currency.label,
+				symbol: item.currency.symbol,
+				amount: item.amount,
+			};
+		});
+
+		userMatchedCurrency = currencies?.find((el) =>
+			selectedCurrency !== null
+				? el.currency === selectedCurrency.currency
+				: el.currency === 'USD'
+		);
+
+		return userMatchedCurrency;
+	}
+}
