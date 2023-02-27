921f98e4f239744262c4e0f480a381d722c2e1ee
Nafi R <saejim91@gmail.com>
2023-02-28 01:09:15 +0600

    styling fixed

--- src/components/Utilities/numberRounder.js
+++ src/components/Utilities/numberRounder.js
@@ -1,6 +1,7 @@
 export const roundToTwoDecimalPlaces = (n) => {
 	let decimalTotal = n.toFixed(2);
-	let [integerPart, decimalPart] = decimalTotal.split('.');
-	let roundedTotal = `${integerPart}.${decimalPart}`;
-	return roundedTotal;
+	// let [integerPart, decimalPart] = decimalTotal.split('.');
+	// let roundedTotal = `${integerPart}.${decimalPart}`;
+	// return roundedTotal;
+	return decimalTotal;
 };
